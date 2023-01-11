9ae46401a7d3773609dbe391dec8c2b4a8d6838e
Gabriel <gabriel.leite.leandro@gmail.com>
2023-01-10 22:52:30 -0300

    feat: get orders & get orders by id routes

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -49,8 +49,60 @@ export async function postOrder(req, res){
 
 export async function getOrders(req, res){
 
+    try{
+        const allOrders = await connectionDB.query("SELECT * FROM orders;");
+        
+        if (!allOrders.rows[0]) {
+            return res.status(400).send([]);
+        }
+
+    return res.send(allOrders.rows).status(200);
+
+    }catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
 }
 
 export async function getOrdersId(req, res){
 
+    const { id } = req.params
+
+    try{
+
+        const orderById = await connectionDB.query('SELECT * FROM orders WHERE id = $1', [id]);
+    
+        if (!orderById.rows[0]) {
+            return res.sendStatus(404);
+        }
+
+        const clientOrderId = await connectionDB.query('SELECT * FROM clients WHERE id = $1', [orderById.rows[0].clientId]);
+        const cakeOrderId = await connectionDB.query('SELECT * FROM cakes WHERE id = $1', [orderById.rows[0].cakeId]);
+
+        const ordersData = {
+            client: {
+                id: clientOrderId.rows[0].id,
+                name: clientOrderId.rows[0].name,
+                address: clientOrderId.rows[0].address,
+                phone: clientOrderId.rows[0].phone,
+            },
+            cake: {
+                id: cakeOrderId.rows[0].id,
+                name: cakeOrderId.rows[0].name,
+                price: cakeOrderId.rows[0].price,
+                description: cakeOrderId.rows[0].description,
+                image: cakeOrderId.rows[0].image,
+            },
+            orderId: orderById.rows[0].id,
+            createdAt: orderById.rows[0].createdAt,
+            quantity: orderById.rows[0].quantity,
+            totalPrice: orderById.rows[0].totalPrice
+        }
+
+    return res.status(200).send(ordersData);
+
+    }catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
 }
